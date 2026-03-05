9448ddf72d1113c7c384711c53f22c5af222d17c
Alfreditann <alfred.ulsberg@gmail.com>
2026-03-05 10:18:02 +0100

    tuff

--- app.py
+++ app.py
@@ -1,5 +1,26 @@
-from FastApi import FastApi, HTTPException
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
-import random 
-
-app = FastApi(title="LOTR Quotes")
+import random
+ 
+app = FastAPI(title="HP quote")
+ 
+sitater = {
+    1: "Yare yare daze.",
+    2: "I, Giorno Giovanna, have a dream.",
+    3: "You thought it was someone else, but it was me, Dio!",
+    4: "Your next line is: 'What?!'",
+    5: "MUDA MUDA MUDA!",
+    6: "ORA ORA ORA!",
+    7: "This must be the work of an enemy Stand!",
+    8: "Good grief...",
+    9: "WRYYYY!",
+    10: "The Joestar bloodline will not fall so easily."
+}
+ 
+class GetQuote(BaseModel):
+    quote: str
+ 
+@app.get("/sitat",response_model=GetQuote)
+def sitat():
+    id = random.randint(1,len(sitater))
+    return {"quote":sitater[id]}
